6e9d90874030a21008db5ef96408b222646f2d5e
MemishKush <MemishKush@users.noreply.github.com>
2019-11-24 22:43:11 +0200

    Implementing services..
    Adding more methods to services

--- src/main/java/com/project/barfinder/service/UserService.java
+++ src/main/java/com/project/barfinder/service/UserService.java
@@ -1,11 +1,24 @@
 package com.project.barfinder.service;
 
 import com.project.barfinder.domain.entities.User;
+import com.project.barfinder.domain.models.service.UserServiceModel;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface UserService extends UserDetailsService {
-    Optional<List<User>> findAllUsers();
+    UserServiceModel addUser(UserServiceModel userServiceModel);
+
+    List<UserServiceModel> findAllUsers();
+
+    UserServiceModel findByUsername(String username);
+
+    UserServiceModel findById(String id);
+
+    UserServiceModel findByEmail(String email);
+
+    UserServiceModel editUser(UserServiceModel userServiceModel);
+
+    void deleteUser(String id);
 }
--- src/main/java/com/project/barfinder/service/UserServiceImpl.java
+++ src/main/java/com/project/barfinder/service/UserServiceImpl.java
@@ -1,11 +1,13 @@
 package com.project.barfinder.service;
 
 import com.project.barfinder.domain.entities.User;
+import com.project.barfinder.domain.models.service.UserServiceModel;
 import com.project.barfinder.repository.UserRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,8 +21,44 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public Optional<List<User>> findAllUsers() {
-        return Optional.ofNullable(userRepository.findAll());
+    public UserServiceModel addUser(UserServiceModel userServiceModel) {
+        return this.modelMapper.map(this.userRepository.save(this.modelMapper.map(userServiceModel,User.class)),UserServiceModel.class);
+    }
+
+    @Override
+    public List<UserServiceModel> findAllUsers() {
+        List<UserServiceModel> serviceModels = new ArrayList<>();
+        List<User> usersFromDb = this.userRepository.findAll();
+        for (User user : usersFromDb) {
+            UserServiceModel mappedServiceModels = this.modelMapper.map(user,UserServiceModel.class);
+            serviceModels.add(mappedServiceModels);
+        }
+        return serviceModels;
+    }
+
+    @Override
+    public UserServiceModel findByUsername(String username) {
+        return this.modelMapper.map(userRepository.findByUsername(username),UserServiceModel.class);
+    }
+
+    @Override
+    public UserServiceModel findById(String id) {
+        return this.modelMapper.map(userRepository.findById(id),UserServiceModel.class);
+    }
+
+    @Override
+    public UserServiceModel findByEmail(String email) {
+        return this.modelMapper.map(userRepository.findByEmail(email),UserServiceModel.class);
+    }
+
+    @Override
+    public UserServiceModel editUser(UserServiceModel userServiceModel) {
+        return this.modelMapper.map(userRepository.saveAndFlush(this.modelMapper.map(userServiceModel,User.class)),UserServiceModel.class);
+    }
+
+    @Override
+    public void deleteUser(String id) {
+    this.userRepository.deleteById(id);
     }
 
     @Override
